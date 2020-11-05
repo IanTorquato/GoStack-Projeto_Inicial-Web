fba8e61d8e5f4d8f85060436ab90509844c097a8
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-11-05 15:47:18 -0300

    Rotas na aplicação

---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Main from './pages/Main'
+import Repository from './pages/Repository'
+
+const Routes = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route component={Main} path="/" exact />
+				<Route component={Repository} path="/repository" />
+			</Switch>
+		</BrowserRouter>
+	)
+}
+
+export default Routes
