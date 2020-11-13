9c556c88b72417fa031a766d01c278bc14a00668
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-11-13 16:19:03 -0300

    Finalizando página Repository

--- src/pages/Repository/styles.jsx
+++ src/pages/Repository/styles.jsx
@@ -41,4 +41,63 @@ const Owner = styled.header`
 	}
 `
 
-export { Loading, Owner }
+const IssueList = styled.ul`
+	border-top: 1px solid #eee;
+	list-style: none;
+	margin-top: 30px;
+	padding-top: 30px;
+
+	li {
+		border: 1px solid #eee;
+		border-radius: 4px;
+		display: flex;
+		padding: 15px 10px;
+
+		& + li {
+			margin-top: 10px;
+		}
+
+		img {
+			border: 1px solid #eee;
+			border-radius: 50%;
+			height: 36px;
+			width: 36px;
+		}
+
+		div {
+			flex: 1;
+			margin-left: 15px;
+
+			strong {
+				font-size: 15px;
+
+				a {
+					color: #333;
+
+					&:hover {
+						color: #7159c1;
+					}
+				}
+
+				span {
+					background-color: #eee;
+					border-radius: 2px;
+					color: #333;
+					font-size: 12px;
+					font-weight: 600;
+					height:20px;
+					margin-left: 10px;
+					padding: 3px 4px;
+				}
+			}
+		}
+
+		p {
+			color: #999;
+			font-size: 12px;
+			margin-top: 5px;
+		}
+	}
+`
+
+export { Loading, Owner, IssueList }
